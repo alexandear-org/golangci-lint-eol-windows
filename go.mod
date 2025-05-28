79fcf4e9effb8a49d313280ba32cd3d078521ccf
Oleksandr Redko <oleksandr.red+github@gmail.com>
2025-05-28 17:57:36 +0300

    remove spew

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module github.com/alexandear-org/golangci-lint-eol-windows
 
 go 1.23
-
-require github.com/davecgh/go-spew v1.1.1 // indirect
--- main.go
+++ main.go
@@ -1,15 +1,3 @@
-// SPDX-FileCopyrightText: Copyright The Authors
-// SPDX-License-Identifier: The Unlicense
-
 package main
 
-import (
-	"fmt"
-
-	"github.com/davecgh/go-spew/spew"
-)
-
-func main() {
-	spew.Dump("a")
-	fmt.Println("hello")
-}
+func main() {}
